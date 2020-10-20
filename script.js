c947b0281d75e90878d9fbcf2bb7d8fcce15ce37
Serghei <lighsnow@gmail.com>
2020-10-20 17:12:03 +0300

    add project

---
+++ script.js
@@ -0,0 +1,34 @@
+const sliders = document.querySelectorAll('.swiper-container');
+
+
+
+sliders.forEach(slider => {
+  let mySwiper = new Swiper(slider, {
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    // slidesPerView: 'auto',
+  });
+});
+
+
+let mySwiper = new Swiper('.swiper-container1', {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  // slidesPerView: 'auto',
+});
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    // This hides the address bar:
+    window.scrollTo(0, 1);
+  }, 0);
+});
+
+// var swiper = new Swiper('.swiper-container', {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
